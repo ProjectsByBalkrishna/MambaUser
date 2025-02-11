f9c1b3e4b11035fc39592388a70f4e21ae8d397f
Balkrishna-1663 <Kishansingh223311@gmail.com>
2025-02-11 23:57:04 +0530

    Added User Model

--- Models/LoginUser.cs
+++ Models/LoginUser.cs
@@ -7,6 +7,13 @@ namespace MambaUser.Models{
         public int UserID { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public string ConfirmPassword {get;set; }
+        public EmailAddressAttribute Email {get;set; }
+        public required long Phone  { get; set; }
+        public bool  IsActive { get; set; }
+        public DateTime CreatedDate { get; set; } =DateTime.Today;
+        public Admin CreatedBy {get;set;}
+        public DateTime ModifiedDate{get;set;}= DateTime.Today;
+        public Admin ModifiedBy{get;set;}
+
     }
 }
